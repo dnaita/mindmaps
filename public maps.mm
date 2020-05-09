2ae90ee01b14d867f25dda5f85c9c5dd020933a9
Deepak Naita <deepaknaita@gmail.com>
2020-05-09 13:38:19 +0530

    Rmeove old data

--- public maps.mm
+++
@@ -1,7 +0,0 @@
-<map version="0.9.0">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1325329896095" ID="ID_1908533411" MODIFIED="1325330314261" TEXT="public maps">
-<node CREATED="1325329916900" ID="ID_137848204" LINK="http://www.mindmeister.com/maps/show/40950677" MODIFIED="1325329916900" POSITION="right" TEXT="mindmeister.com &gt; Maps &gt; Show &gt; 40950677"/>
-<node CREATED="1325330315514" ID="ID_877692438" LINK="http://www.mindmeister.com/maps/show/12213323" MODIFIED="1325330315514" POSITION="left" TEXT="mindmeister.com &gt; Maps &gt; Show &gt; 12213323"/>
-</node>
-</map>
